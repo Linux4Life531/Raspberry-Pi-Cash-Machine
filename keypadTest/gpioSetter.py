118695461c98a17123c7326b217ede667ccc0822
Linux4Life531 <shiney9y@gmail.com>
2020-08-24 16:17:24 +0100

    Removing not needed files

--- keypadTest/gpioSetter.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-
-while True:
-    pin = int(input("ENTER PIN NUMBER: "))
-    value = int(input("ENTER PIN VALUE (1 OR 0): "))
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, value)
